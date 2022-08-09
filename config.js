9347702cfc24aa4eed4af10df7288f1d79637168
Ariok12 <73320597+Ariok12@users.noreply.github.com>
2022-08-10 00:46:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
+global.Owner = ['917076572177']
+global.OwnerNumber = ['917076572177']
+global.ownertag = ['917076572177']
 global.BotName = "Miku"
 global.packname = "Miku bot"
 global.author = "By: Fantox"
